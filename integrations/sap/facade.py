daa3fb4737ac8ed32c91bc51a53440616247449d
Brayan Leon <bleon2845@gmail.com>
2026-01-15 21:56:32 -0500

    Update Code

--- integrations/sap/facade.py
+++ integrations/sap/facade.py
@@ -1,16 +1,23 @@
 from integrations.sap.sap_gui import SapGUI
 from integrations.sap.create_order import CreateOrder
+from integrations.sap.save_docs import SaveDocs
 
 class SapFacade:
 
     def __init__(self, sap_gui: SapGUI):
         self.sap = sap_gui
-        self._create_order = None
+        self._create_order: CreateOrder | None = None
+        self._save_docs: SaveDocs | None = None
 
     #------------ Connection and Login -------------
-    def open_and_connect(self,system_name: str = None,index: int = None) -> None:
+    def _ensure_session(self, system_name: str = None,index: int = None):
+        if self.sap.session:
+            return
         self.sap.open_sap()
         self.sap.connect_sap(system_name=system_name, index=index)
+
+        if not self.sap.session:
+            raise RuntimeError("unable to establish SAP session.")
 
     def login(self,username: str, password: str,system_name: str = None) -> bool:
         self.sap.open_sap()
@@ -26,10 +33,8 @@ class SapFacade:
         return True
 
     #------------ Create Order ME21N -------------
-    def create_orders(self, excel_path: str):
-        if not self.sap.session:
-            raise RuntimeError("SAP session not available. Please login first.")
-
+    def create_orders(self, excel_path: str, file_folder: str):
+        self._ensure_session()
         if not self._create_order:
             self._create_order = CreateOrder(self.sap.session)
 
@@ -37,24 +42,21 @@ class SapFacade:
 
     #------------ Save PDF MB02 SP01------------- 
     def print_documents(self, excel_path: str, output_folder: str):
-        if not self.sap.session:
-            raise RuntimeError("SAP session not available. Please login first.")
+        self._ensure_session()
 
-        if not self.save_docs_service:
+        if not self._save_docs_service:
             raise RuntimeError("SaveDocs service not initialized.")
 
-        self.save_docs_service.print_documents(excel_path, output_folder)
+        self._save_docs_service.print_documents(excel_path, output_folder)
 
     #------------ Create Identity Document -------------
     def save_docs(self, df_print, output_folder):
-        if not self.session:
-            raise RuntimeError("SAP session not available. Please login first.")
-
-        if not self.save_docs_service:
+        self._ensure_session()
+        
+        if not self._save_docs_service:
             raise RuntimeError("SaveDocs service not initialized.")
-
+        
         self.save_docs_service.save_docs(df_print, output_folder)
-
 
 
 
